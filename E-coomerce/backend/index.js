8fa18c9f0d5616f88e14ef534498227c082a4d8f
dox404 <muktarul.hoque@hotmail.com>
2023-03-18 00:45:16 +0530

    50% project done

--- E-coomerce/backend/index.js
+++ E-coomerce/backend/index.js
@@ -1,9 +1,10 @@
-const express=require('express')
+const express = require('express')
 require('./db/config')
-const cors=require('cors')
-const User=require('./db/User')
-const jwt =require('jsonwebtoken')
-
+const cors = require('cors')
+const User = require('./db/User')
+const Product = require('./db/Product')
+const jwt = require('jsonwebtoken')
+const PORT = 5000;
 // const connectDB=async()=>{
 //     mongoose.connect('mongodb://localhost:27017/e-comm');
 //     const productSchema=new mongoose.Schema({})
@@ -14,7 +15,7 @@ const jwt =require('jsonwebtoken')
 // connectDB()
 
 
-const app=express()
+const app = express()
 app.use(express.urlencoded({ extended: false }))
 
 
@@ -27,22 +28,61 @@ app.use(cors())
 
 
 
-//api
-app.post("/register",async(req,res)=>{
-    let user=new User(req.body)
-    const data = await user.save();
-    const payload = {
-        user: data,
-        token: jwt.sign({secret:"Mani"},"mmmmmmm"),
-        message: 'User Registered successfully'
-    }
-    res.status(201)
-    res.send(payload)
-})
-app.get("/",(req,res)=>{
-    res.send("Hello from server")
+app.post("/add-product", async (req, res) => {
+    let product = new Product(req.body)
+    let result = await product.save()
+    res.send(result)
 })
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000 ") 
+//api
+app.post("/register", async (req, res) => {
+    console.log(req.body)
+    let user = new User(req.body)
+    // console.log(user)
+    let data = await user.save();
+    // console.log(data)
+    const payload = {
+        user: data,
+        token: jwt.sign({ secret: "Mani" }, "mmmmmmm"),
+        message: 'User Registered successfully'
+    }
+    // console.log(data)
+    res.status(201)
+    res.send(payload)
+    //// data=data.toObject()
+    // delete data.password
+
+})
+
+app.post('/login', async (req, res) => {
+    if (req.body.email && req.body.password) {
+        let user = await User.findOne(req.body).select("-password")
+        // console.log(user)
+        if (user) {
+            // const payload = {
+            //     user: user,
+            //     token: jwt.sign({secret:"Mani"},"mmmmmmm"),
+            //     message: 'User Registered successfully'
+            // }
+            res.send(user)
+
+
+        } else {
+            res.send({ result: "User not found" })
+        }
+    } else {
+        res.send("please enter all feilds..!")
+    }
+
+})
+
+
+
+app.get("/", (req, res) => {
+    const user = localStorage.getItem('user')
+    res.send(user)
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
 })
